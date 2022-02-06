82d246e9f359e7a181173c7463a257dde06eb412
sultan2503 <74846378+sultan2503@users.noreply.github.com>
2022-02-06 20:04:33 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,15 @@
 import requests
 
-url = "https://35.170.39.58/api/v1/auth"
+url = "https://35.187.30.58/api/v1/auth"
 
 payload={}
 headers = {
   'Authorization': 'Basic YXM6QXNAMTIz'
+#-- YXM6QXNAMTIz is combination of (username:password) with base64 encoding
 }
 
 response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+#--Disable SSL: CERTIFICATE_VERIFY with verify=False
 
 print(response.text)
